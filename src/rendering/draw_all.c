c8be653f3a86adc51aaee5744e5c3c61ed28cc98
Jaeyoung Choi <jchoi@c6r2s8.42seoul.kr>
2023-02-02 16:07:29 +0900

    light_complete

--- src/rendering/draw_all.c
+++ src/rendering/draw_all.c
@@ -6,7 +6,7 @@
 /*   By: seoyoo <seoyoo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 10:30:00 by seoyoo            #+#    #+#             */
-/*   Updated: 2023/02/02 02:52:15 by jchoi            ###   ########.fr       */
+/*   Updated: 2023/02/02 13:05:02 by jchoi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,21 @@ void	draw_all(t_mlx *mlx_, t_img *img_, t_objs *objs_)
 	t_screen	screen_;
 	t_line3		sight_;
 	t_dot3		onscreen_;
-//	t_color		pixel_clr;
-	size_t		idx[2];
+	size_t		i[2];
 
 	onscreen_ = set_screen(&screen_, objs_->camera_);
-	idx[y_] = 0;
-	while (idx[y_] < WINDOW_HEIGHT_)
+	i[y_] = 0;
+	while (i[y_] < WINDOW_HEIGHT_)
 	{
-		idx[x_] = 0;
-		while (idx[x_] < WINDOW_WIDTH_)
+		i[x_] = 0;
+		while (i[x_] < WINDOW_WIDTH_)
 		{
 			sight_ = line3_by_dots(objs_->camera_.pos_, onscreen_);
-			put_pixel(img_, idx[y_], idx[x_], process_pixel(objs_, sight_));
+			put_pixel(img_, i[y_], i[x_], process_pixel(objs_, sight_, i[y_]));
 			onscreen_ = add_vec3(onscreen_, screen_.hor_unit_);
-			idx[x_]++;
+			i[x_]++;
 		}
-		onscreen_ = times_vec3(screen_.ver_unit_, ++idx[y_]);
+		onscreen_ = times_vec3(screen_.ver_unit_, ++i[y_]);
 		onscreen_ = add_vec3(screen_.start_, onscreen_);
 	}
 	mlx_put_image_to_window(mlx_->mlx_ptr_, mlx_->win_ptr_, img_->img_ptr_, 0, 0);
@@ -119,7 +118,7 @@ t_vec3	get_normal(t_pvec3 pos_, t_line3 sight_, t_figure *fg_)
 //		return ();
 }
 
-t_color	process_pixel(t_objs *objs_, t_line3 sight_)
+t_color	process_pixel(t_objs *objs_, t_line3 sight_, size_t y)
 {
 	t_cpnt	contact_;
 	t_rgb	delta_;
@@ -135,35 +134,21 @@ t_color	process_pixel(t_objs *objs_, t_line3 sight_)
 		while (i < objs_->light_cnt_)
 			get_light(objs_->lights_[i++], sight_, &contact_);
 		delta_ = add_vec3(contact_.rgb_, objs_->ambient_.increment_);
+		delta_ = times_vec3(delta_, 1.0 / 256);
 		contact_.rgb_ = mul_vec3(delta_, color_to_rgb(contact_.fg_->clr_));
-		contact_.rgb_ = times_vec3(contact_.rgb_, 1 / 256.0);
 		return (rgb_to_color(contact_.rgb_));
 	}
 	else
 	{
-		tval = 0.5 * (sight_.dir_.e[y_] + 1.0);
-		printf("tval : %f\n", tval);
-		//return (0xFFFFFF * (1 - tval) + 0x80B3FF * tval);
-		return (0xFFFFFF * (1 - tval) + 0x000000 * tval);
-	}//		return (empty_air(tval, objs_->bgclr1_ objs_->bgclr2_));
+		tval = ((double)y / WINDOW_HEIGHT_);
+		return (rgb_to_color(times_vec3(add_vec3(times_vec3(init_vec3(1, 1, 1)
+			, 1 - tval), times_vec3(init_vec3(0.5, 0.7, 1.0), tval)), 256)));
+	}
 }
-
-
-// init_vec3(1, 1, 1), init_vec3(0.5, 0.7, 1.0) 이 두가지 색깔은 obj에 들어있는것으로 하자.
-/*
-   t_color	empty_air(double t, t_rgb clr1_, t_rgb clr2_)
-{
-	t_rgb	gradation_;
-	return (rgb_to_color(add_vec3(times_vec3(clr1_, 1 - t), times_vec3(clr2_, t))));
-
-//return (vplus(vmult(color3(1, 1, 1), 1.0 - t), vmult(color3(0.5, 0.7, 1.0), t)));
-	return ();
-}
-*/
 
 # define KSN_ 64
 # define KS_ 0.5
-# define LUMEN 3
+# define LUMEN 2
 
 void	get_light(t_light light_, t_line3 sight_, t_cpnt *contact_)
 {
@@ -174,16 +159,16 @@ void	get_light(t_light light_, t_line3 sight_, t_cpnt *contact_)
     double	specular;
 	double	increment;
 
+	(void)sight_;
+	(void)specular;
+	(void)tmp_;
     raydir_ = normalize_vec3(sub_vec3(light_.light_point_, contact_->pos_));
 	diffuse = fmax(dot_product(contact_->normal_, raydir_), 0.0);
-	// 여기까지가 diffuse입니다.
-	
-	tmp_ = times_vec3(normal_vec3(raydir_, contact_->normal_), -2);
+	//increment = diffuse * light_.ratio_ * LUMEN;
+	tmp_ = times_vec3(tangent_vec3(raydir_, contact_->normal_), -2);
 	raydir_ = add_vec3(raydir_, tmp_);
 	specular = pow(fmax(dot_product(sight_.dir_, raydir_), 0.0), KSN_);
-	// 여기까지가 specular입니다.	
 	increment = (specular * KS_ + diffuse) * light_.ratio_ * LUMEN;
-	// 광도를 곱해줍니다.
 	light_rgb_ = times_vec3(color_to_rgb(light_.color_), increment);
 	contact_->rgb_ = add_vec3(contact_->rgb_, light_rgb_);
 }
