a540c4a46eb4373d277ca73633b20642084dad85
Seojun Yoo <seoyoo@c5r1s4.42seoul.kr>
2023-02-06 20:48:59 +0900

    fixed: \n segfalut error fixed

--- src/input_parsing/parse_input.c
+++ src/input_parsing/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: seoyoo <seoyoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 15:41:10 by seoyoo            #+#    #+#             */
-/*   Updated: 2023/02/06 20:15:38 by seoyoo           ###   ########.fr       */
+/*   Updated: 2023/02/06 20:48:14 by seoyoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static void	count_elements(t_objs *objs, int fd, int input_cnt[]);
 static void	plus_one_to_match(char *gnl_result, int input_cnt[]);
 static void	set_elements(t_objs *objs, int fd);
+static char	**get_next_line_and_split(int fd);
 
 void	parse_input(t_objs *objs, char *src_file)
 {
@@ -84,17 +85,13 @@ static void	set_elements(t_objs *objs, int fd)
 {
 	char	**splitted_str;
 	int		idx[2];
-	char	*gnl_result;
 
 	objs->lights_ = my_calloc(objs->light_cnt_, sizeof(t_light));
 	objs->figures_ = my_calloc(objs->figure_cnt_, sizeof(t_figure));
 	ft_bzero(idx, sizeof(int) * 2);
 	while (true)
 	{
-		gnl_result = get_next_line_without_new_line(fd);
-		if (ft_strlen(gnl_result) == 0)
-			continue ;
-		splitted_str = my_split(gnl_result, ' ');
+		splitted_str = get_next_line_and_split(fd);
 		if (splitted_str == NULL)
 			break ;
 		else if (my_strcmp(splitted_str[0], "A") == 0)
@@ -105,7 +102,26 @@ static void	set_elements(t_objs *objs, int fd)
 			scan_light(&objs->lights_[idx[0]++], splitted_str);
 		else
 			scan_figures(&objs->figures_[idx[1]++], splitted_str);
-		free(gnl_result);
 		free_str_arr(splitted_str);
 	}
+}
+
+static char	**get_next_line_and_split(int fd)
+{
+	char	*gnl_result;
+	char	**splitted_str;
+
+	while (true)
+	{
+		gnl_result = get_next_line_without_new_line(fd);
+		if (gnl_result == NULL)
+			return (NULL);
+		else if (*gnl_result == '\0')
+			free(gnl_result);
+		else
+			break ;
+	}
+	splitted_str = my_split(gnl_result, ' ');
+	free(gnl_result);
+	return (splitted_str);
 }
